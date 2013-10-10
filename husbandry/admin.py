985745a8b6845ce3377a407d46698418123f794a
Justin Kiggins <justin.kiggins@gmail.com>
2013-10-10 08:47:44 -0700

    more details to husbandry admin page

--- husbandry/admin.py
+++ husbandry/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from husbandry.models import Subject
+from husbandry.models import Subject, Observation
 
 class SubjectAdmin(admin.ModelAdmin):
     list_display = (
@@ -7,4 +7,25 @@ class SubjectAdmin(admin.ModelAdmin):
         'sex',
         'origin',
         )
+    search_fields = [
+        'name',
+        'origin',
+        'observation_set__notes'
+        ]
+    list_filter = ('origin','sex')
 admin.site.register(Subject,SubjectAdmin)
+
+class ObservationAdmin(admin.ModelAdmin):
+    list_display = (
+        'datetime',
+        'subject',
+        'weight',
+        'notes',
+        )
+    search_fields = [
+        'subject__name',
+        'subject__origin',
+        'notes'
+        ]
+    list_filter = ('datetime','subject')
+admin.site.register(Observation,ObservationAdmin)
